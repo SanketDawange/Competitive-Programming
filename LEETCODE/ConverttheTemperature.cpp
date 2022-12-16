495088555f7dda67f9a118af4be125029dd89bab
Sanket Dawange <99337324+SanketDawange@users.noreply.github.com>
2022-12-16 20:55:54 +0530

    Create ConverttheTemperature.cpp

---
+++ LEETCODE/ConverttheTemperature.cpp
@@ -0,0 +1,15 @@
+/*
+LINK : https://leetcode.com/problems/convert-the-temperature/
+SOLUTION :
+*/
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans(0);
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius*1.80 + 32.00;
+        ans.push_back(kelvin);
+        ans.push_back(fahrenheit);
+        return ans;
+    }
+};
