c68165d39df05c44315d902046da3a4011fc5f1b
Sanket Dawange <99337324+SanketDawange@users.noreply.github.com>
2022-12-16 21:38:39 +0530

    Create smallest-even-multiple.cpp

---
+++ LEETCODE/smallest-even-multiple.cpp
@@ -0,0 +1,10 @@
+/*
+https://leetcode.com/problems/smallest-even-multiple/
+*/
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n&1) return n*2;
+        return n;
+    }
+};
