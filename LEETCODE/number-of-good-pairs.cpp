d19b86d4e3d53a268dd5cd60388eb73b53961cd7
Sanket Dawange <99337324+SanketDawange@users.noreply.github.com>
2022-12-16 21:00:58 +0530

    Create number-of-good-pairs.cpp

---
+++ LEETCODE/number-of-good-pairs.cpp
@@ -0,0 +1,17 @@
+/*
+LINK : https://leetcode.com/problems/number-of-good-pairs/
+SOLN :
+*/
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i] == nums[j])
+                    ans ++;
+            }
+        }
+        return ans;
+    }
+};
