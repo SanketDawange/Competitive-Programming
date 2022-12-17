c75fc61b0340415b290c3f04cab6ad449d16864c
Sanket Dawange <99337324+SanketDawange@users.noreply.github.com>
2022-12-17 11:46:18 +0530

    Create TODOLIST.cpp

---
+++ CODECHEF/ARRAYS/TODOLIST.cpp
@@ -0,0 +1,131 @@
+/*
+https://www.codechef.com/problems/TODOLIST
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define inf 1e18
+#define iinf INT_MIN
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define parity_check __builtin_parity //1=odd,0=even
+#define set_bits __builtin_popcountll
+#define leading_zeros __builtin_clzll //00010 = 3
+#define trailing_zeros __builtin_ctzll //00010 = 1
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vii vector<int>
+#define pll pair<ll,ll>
+#define vpii vector<pair<int,int>>
+#define vll vector<ll>
+#define eb emplace_back
+#define vpll vector<pair<ll,ll>>
+#define vch vector<char>
+#define vvll vector<vector<ll>>
+#define min_heap priority_queue <int, vector<int>, greater<int>>
+#define max_heap priority_queue <int>
+#define rep(var , start , end) for(int var = start ; var < end ; var++)
+#define rrep(var , start , end) for(int var = start ; var >= end ; var--)
+#define py cout<<"YES"<<nline
+#define pn cout<<"NO"<<nline
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef __int128 ell;
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update > pbds;
+/*
+find_by_order(k) = kth element in set (k = index)
+order_of_key(k) = Number of items strictly smaller than k
+*/
+
+#ifndef ONLINE_JUDGE
+#define deb(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define deb(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll lcm(ll a, ll b) {return (1ll * a * b) / gcd(a, b) ;}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+ll ncr(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+ll npr(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; return (((val1 * val2) % m));}
+
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);}
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+//////////////   START  ///////////////
+
+void solve() {
+    int p1, p2, p3, p4;
+    cin >> p1 >> p2 >> p3 >> p4;
+    int ans = 0;
+    if(p1>=10) ans ++;
+    if(p2>=10) ans ++;
+    if(p3>=10) ans ++;
+    if(p4>=10) ans ++;
+    cout << ans << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio();
+
+    int test_cases = 1 ;
+    // cin >> test_cases ;
+
+    while (test_cases--) {
+        solve();
+    }
+
+}
