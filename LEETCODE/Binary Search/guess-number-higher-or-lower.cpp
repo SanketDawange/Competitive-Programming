cc04b71254c854fc0f13b6bf27ef867169115bcb
Sanket Dawange <99337324+SanketDawange@users.noreply.github.com>
2022-12-20 19:51:19 +0530

    Create guess-number-higher-or-lower.cpp

---
+++ LEETCODE/Binary Search/guess-number-higher-or-lower.cpp
@@ -0,0 +1,26 @@
+/*
+https://leetcode.com/problems/guess-number-higher-or-lower/
+*/
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 0;
+        int high = n;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(guess(mid)==0) return mid;
+            if(guess(mid)==-1) high = mid-1;
+            else low = mid+1;
+        }
+        return high;
+    }
+};
