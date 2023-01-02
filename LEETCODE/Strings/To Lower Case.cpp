76086c63cd63b3d60af4b16f99d4f58a1b656df4
Sanket Dawange <99337324+SanketDawange@users.noreply.github.com>
2023-01-02 16:34:26 +0530

    Create To Lower Case.cpp

---
+++ LEETCODE/Strings/To Lower Case.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] <= 90 && s[i]>=65) s[i] += 32;
+        } return s;
+    }
+};
